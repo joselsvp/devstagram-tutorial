a94e14ad0a3ed56be762234f133490d1694aa004
Jose Villanueva <lv428694@gmail.com>
2022-05-25 23:26:40 -0500

    add: form styles register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,42 @@
 @extends('layouts.app')
 
 @section('titulo', 'Registrate en DevStagram')
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{asset('img/registrar.jpg')}}" alt="registro">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="">
+                <div class="mb-5">
+                    <label for="name" class="mb-1 block uppercase text-gray-500 font-bold">Nombre</label>
+                    <input type="text" id="name" name="name" placeholder="Ingrese su(s) nombre(s)" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <div class="mb-5">
+                    <label for="username" class="mb-1 block uppercase text-gray-500 font-bold">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Ingrese nombre de usuario" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-1 block uppercase text-gray-500 font-bold">Username</label>
+                    <input type="email" id="email" name="email" placeholder="Ingrese correo electrónico" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="mb-1 block uppercase text-gray-500 font-bold">Contraseña</label>
+                    <input type="password" id="password" name="password" placeholder="Ingrese contraseña" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-1 block uppercase text-gray-500 font-bold">Confirmar Contraseña</label>
+                    <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirma contraseña" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
