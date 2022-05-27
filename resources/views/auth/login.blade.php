6226c466a3b0780eafa5423ccec1020daa945795
Jose Villanueva <lv428694@gmail.com>
2022-05-27 02:22:42 -0500

    add: login view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('titulo', 'Inicia sesión en DevStagram')
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{asset('img/login.jpg')}}" alt="login">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form novalidate>
+                @csrf
+                <div class="mb-5">
+                    <label for="email" class="mb-1 block uppercase text-gray-500 font-bold">Correo</label>
+                    <input type="email" id="email" name="email" placeholder="Ingrese correo electrónico" class="border p-3 w-full rounded-lg
+                    @error('email') border-gray-600 @enderror" value="{{old('email')}}">
+                    @error('email')
+                    <p class="bg-gray-600 text-white my-2 rounded-lg text-sm p-3 mt-2 text-center">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="mb-1 block uppercase text-gray-500 font-bold">Contraseña</label>
+                    <input type="password" id="password" name="password" placeholder="Ingrese contraseña" class="border p-3 w-full rounded-lg @error('password') border-gray-600 @enderror">
+                    @error('password')
+                    <p class="bg-gray-600 text-white my-2 rounded-lg text-sm p-3 mt-2 text-center">{{$message}}</p>
+                    @enderror
+                </div>
+
+
+                <input type="submit" value="Iniciar sesión" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -22,5 +23,7 @@ Route::get('/', function () {
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
 
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
