01d759d51fb6f6a91ee3d5f7b45d4c07bc50f7ff
Jose Villanueva <lv428694@gmail.com>
2022-05-27 20:41:17 -0500

    add: remember sesion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,9 @@
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input type="checkbox" name="remember"> <label for="remember" class="text-sm text-gray-500 ">Mantener mi sesión abierta</label>
+                </div>
 
                 <input type="submit" value="Iniciar sesión" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
