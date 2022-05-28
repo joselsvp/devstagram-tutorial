74af835f58e41659cd139344415f644e1826fccc
Jose Villanueva <lv428694@gmail.com>
2022-05-27 21:08:55 -0500

    update: username dashboard

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     }
 
     public function index(User $user){
-        return view('dashboard');
+        return view('dashboard',compact('user'));
     }
 
 }
