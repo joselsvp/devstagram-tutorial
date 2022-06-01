86af6674d02371304a0772ba40f75a20d05d0b1a
Jose Villanueva <lv428694@gmail.com>
2022-05-31 21:16:31 -0500

    update: create directory if does not exist

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 
@@ -14,9 +15,15 @@ class ImagenController extends Controller
         $nombreImagen = Str::uuid() . "." . $imagen->extension();
         $imagenServidor = Image::make($imagen);
         $imagenServidor->fit(1000,1000);
+        $path = public_path('uploads');
+
+        if(!File::isDirectory($path)){
+            File::makeDirectory($path, 0777, true, true);
+        }
         $imagenPath = public_path('uploads') . '/' . $nombreImagen;
 
         $imagenServidor ->save($imagenPath);
+
 
         return response()->json(['imagen' => $nombreImagen]);
     }
