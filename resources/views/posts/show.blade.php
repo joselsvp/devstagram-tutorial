1bd8f3fcf9fee1acd1dd04dfb05b3c339f83569e
Jose Villanueva <lv428694@gmail.com>
2022-06-09 13:27:29 -0500

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,6 @@
 @endsection
 
 @section('contenido')
-
+    <p class="text-center">{{$post->descripcion}}</p>
+    <img style="width: 20%" src="{{asset('uploads') . '/' . $post->imagen}}" alt="{{$post->imagen}}">
 @endsection
