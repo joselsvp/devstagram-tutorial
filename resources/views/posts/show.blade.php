57cc7aec076b8fe39d55a5458289430783e1d4b8
Jose Villanueva <lv428694@gmail.com>
2022-06-09 13:58:03 -0500

    update: delete post id

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,4 +7,15 @@
 @section('contenido')
     <p class="text-center">{{$post->descripcion}}</p>
     <img style="width: 20%" src="{{asset('uploads') . '/' . $post->imagen}}" alt="{{$post->imagen}}">
+
+    @auth
+        @if($post->user_id === auth()->user()->id)
+            <form action="{{route('posts.destroy', $post)}}" method="POST">
+                @method('DELETE') {{--METODO SPOOFING--}}
+                @csrf
+                <input type="submit" value="Eliminar publicación" class="bg-red-500 hover:text-gray-600 p-2 rounded text-white font-bold mt-4 cursor-pointer">
+            </form>
+        @endif
+    @endauth
+
 @endsection
